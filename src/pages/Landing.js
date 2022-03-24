42957e0873ad520ba82e5117b3ab457a6c6f473c
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-03-25 06:42:16 +0800

    Styled Components - Intro

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
+import styled from 'styled-components';
+
+const Button = styled.button`
+    background: red;
+    color: white;
+    font-size: 1rem;
+`
 
 
 const landing = () => {
@@ -11,6 +18,7 @@ const landing = () => {
         </nav>
         <div className="container page">
             <div className='info'>
+                <Button>Submit</Button>
                 <h1>Job <span>Tracking</span> App</h1>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                 <button className="btn btn-hero">Login / Register</button>

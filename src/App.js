dedd5cc0acda8b345cda16c9c4ce74f4aadc99dd
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-03-25 06:30:00 +0800

    Landing Page - Structure

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import Landing from "./pages/Landing";
 function App() {
   return (
     <div>
-     <h1>Jobify</h1>
-    <Landing/>
+        <Landing/>
    </div>
-   
   );
 }
 
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -5,9 +5,19 @@ import main from '../assets/images/main.svg';
 
 const landing = () => {
   return (
-    <div>
-        <h2>Landing Page</h2>
-    </div>
+    <main>
+        <nav>
+            <img src={logo} alt="jobify" className='logo'></img>
+        </nav>
+        <div className="container page">
+            <div className='info'>
+                <h1>Job <span>Tracking</span> App</h1>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <button className="btn btn-hero">Login / Register</button>
+            </div>
+        </div>
+        <img src={main} alt="job hunt" className='img main-img'/>
+    </main>
   )
 }
 

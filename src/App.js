c50332e2ffcf93012fbda1439ac71b39a53a4e5d
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-03-25 07:36:57 +0800

    React Router Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Landing from "./pages/Landing";
 function App() {
   return (
-    <div>
-        <Landing/>
-   </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Dashboard</div>} />
+        <Route path="/register" element={<div>Register</div>} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/*" element={<div>Error 404</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

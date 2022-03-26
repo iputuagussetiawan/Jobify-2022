63123905eccd19f666d7705975f6a87e27076719
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-03-26 10:59:47 +0800

    Reducer

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -1,4 +1,6 @@
-import React, { useState, useReducer, useContext } from "react";
+import React, { useReducer, useContext } from "react";
+import reducer from "./reducer";
+
 export const initialState = {
 	isLoading: false,
 	showAlert: false,
@@ -8,7 +10,7 @@ export const initialState = {
 
 const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
-	const [state, setState] = useState(initialState);
+	const [state, dispatch] = useReducer(reducer, initialState);
 
 	return (
 		<AppContext.Provider

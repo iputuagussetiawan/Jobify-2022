b6af96454df6bc068ed85fcca4cc2fba669bf1fb
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-03-26 08:24:26 +0800

    Page Structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Landing from "./pages/Landing";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import{ Dashboard,Register,Landing,Error } from './pages'
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <Link to="/">Dashboard</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/landing">Home</Link>
-      </nav>
       <Routes>
-        <Route path="/" element={<div>Dashboard</div>} />
-        <Route path="/register" element={<div>Register</div>} />
-        <Route path="/landing" element={<Landing />} />
-        <Route path="/*" element={<div>Error 404</div>} />
+        <Route path="/" element={<Dashboard/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/landing" element={<Landing/>} />
+        <Route path="/*" element={<Error/>} />
       </Routes>
     </BrowserRouter>
   );

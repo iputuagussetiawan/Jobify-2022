6e58da3d79453e736fb1ea9c1add9d2b629a5200
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-03-26 08:32:13 +0800

    Error Page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import img from '../assets/images/not-found.svg';
+import Wrapper from '../assets/wrappers/ErrorPage';
 
 const Error = () => {
   return (
-    <h1>Error</h1>
+    <Wrapper className='full-page'>
+      <div>
+        <img src={img} alt='Not Found'/>
+        <h3>Ohh! page not found</h3>
+        <p> We Can't seem to find the page you're looking for</p>
+      </div>
+    </Wrapper>
   )
 }
 

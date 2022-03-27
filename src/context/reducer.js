41d6b7da7fce004bedb1601ee01a99cf0e7a6872
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-03-27 18:34:55 +0800

    Toggle Sidebar

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,16 +1,10 @@
 import { 
 	DISPLAY_ALERT,
 	CLEAR_ALERT, 
-	REGISTER_USER_BEGIN,
-	REGISTER_USER_SUCCESS,
-	REGISTER_USER_ERROR,
-	LOGIN_USER_BEGIN,
-	LOGIN_USER_SUCCESS,
-	LOGIN_USER_ERROR,
 	SETUP_USER_BEGIN,
 	SETUP_USER_SUCCESS,
-	SETUP_USER_ERROR 
-
+	SETUP_USER_ERROR,
+	TOGGLE_SIDEBAR
 } from "./actions";
 
 const reducer=(state,action)=>{
@@ -30,62 +24,6 @@ const reducer=(state,action)=>{
 			alertType: "",
 			alertText: "",
 		};
-	}
-
-	if(action.type===REGISTER_USER_BEGIN){
-		return { ...state, isLoading:true }
-	}
-
-	if(action.type===REGISTER_USER_SUCCESS){
-		return { 
-			...state, 
-			isLoading:false,
-			token:action.payload.token, 
-			user:action.payload.user,
-			userLocation:action.payload.location,
-			jobLocation:action.payload.location,
-			showAlert:true,
-			alertType:'success',
-			alertText:'User Created! Redirecting...',
-		}
-	}
-
-	if(action.type===REGISTER_USER_ERROR){
-		return { 
-			...state, 
-			isLoading:false,
-			showAlert:true,
-			alertType:'danger',
-			alertText:action.payload.msg,
-		}
-	}
-
-	if(action.type===LOGIN_USER_BEGIN){
-		return { ...state, isLoading:true }
-	}
-
-	if(action.type===LOGIN_USER_SUCCESS){
-		return { 
-			...state, 
-			isLoading:false,
-			token:action.payload.token, 
-			user:action.payload.user,
-			userLocation:action.payload.location,
-			jobLocation:action.payload.location,
-			showAlert:true,
-			alertType:'success',
-			alertText:'Login Success! Redirecting...',
-		}
-	}
-
-	if(action.type===LOGIN_USER_ERROR){
-		return { 
-			...state, 
-			isLoading:false,
-			showAlert:true,
-			alertType:'danger',
-			alertText:action.payload.msg,
-		}
 	}
 
 	if(action.type===SETUP_USER_BEGIN){
@@ -114,6 +52,10 @@ const reducer=(state,action)=>{
 			alertType:'danger',
 			alertText:action.payload.msg,
 		}
+	}
+
+	if (action.type === TOGGLE_SIDEBAR) {
+		return { ...state, showSidebar: !state.showSidebar };
 	}
 }
 

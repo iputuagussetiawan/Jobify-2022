0a2114f6465ad4008d88a1ad77241420559400d1
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-03-27 19:02:37 +0800

    Setup Links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,33 +10,31 @@ function Navbar() {
 	const { user, logoutUser, toggleSidebar } = useAppContext();
   return (
     <Wrapper>
-			<div className="nav-center">
-				<button
-					className="toggle-btn"
-					onClick={toggleSidebar}
-				>
-					<FaAlignLeft />
+		<div className="nav-center">
+			<button
+				className="toggle-btn"
+				onClick={toggleSidebar}
+			>
+				<FaAlignLeft />
+			</button>
+			<div>
+				<Logo />
+				<h3 className="logo-text">dashboard</h3>
+			</div>
+			<div className="btn-container">
+				<button className='btn' onClick={() => setShowLogout(!showLogout)}>
+					<FaUserCircle />
+					{user.name}
+					<FaCaretDown />
 				</button>
-
-				<div>
-					<Logo />
-					<h3 className="logo-text">dashboard</h3>
-				</div>
-
-				<div className="btn-container">
-					<button className='btn' onClick={() => setShowLogout(!showLogout)}>
-						<FaUserCircle />
-						{user.name}
-						<FaCaretDown />
-					</button>
-					<div className={showLogout ? 'dropdown show-dropdown' : 'dropdown'}>
-					<button onClick={logoutUser} className="dropdown-btn">
-						logout
-					</button>
-					</div>
+				<div className={showLogout ? 'dropdown show-dropdown' : 'dropdown'}>
+				<button onClick={logoutUser} className="dropdown-btn">
+					logout
+				</button>
 				</div>
 			</div>
-		</Wrapper>
+		</div>
+	</Wrapper>
   )
 }
 

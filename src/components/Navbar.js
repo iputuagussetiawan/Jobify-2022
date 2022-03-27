b4f375188466a8ce377c6c8ce2210e31bb1a901c
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-03-27 18:06:39 +0800

    Navbar Structure - Complete

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Wrapper from '../assets/wrappers/Navbar'
+
+function Navbar() {
+  return (
+    <Wrapper>
+      <h4>Navbar</h4>
+    </Wrapper>
+    
+  )
+}
+
+export default Navbar
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Logo} from '../components';
 import main from '../assets/images/main.svg';
 import Wrapper from '../assets/wrappers/LandingPage';
+import { Link } from 'react-router-dom'
 
 const landing = () => {
   return (
@@ -13,7 +14,9 @@ const landing = () => {
             <div className='info'>
                 <h1>Job <span>Tracking</span> App</h1>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                <button className="btn btn-hero">Login / Register</button>
+                <Link to='/register' className='btn btn-hero'>
+                  Login/Register
+                </Link>
             </div>
             <img src={main} alt="job hunt" className='img main-img'/>
         </div>

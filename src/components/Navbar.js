aac2d8b1b39939726e3129f81de1843c6e853ce5
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-03-27 18:21:01 +0800

    Navbar Structure - Complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,45 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/Navbar'
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
+import { useAppContext } from "../context/appContext";
+import Logo from "./Logo";
 
 function Navbar() {
   return (
     <Wrapper>
-      <h4>Navbar</h4>
-    </Wrapper>
-    
+			<div className="nav-center">
+				<button
+					className="toggle-btn"
+					onClick={() => console.log("toggle sidebar")}
+				>
+					<FaAlignLeft />
+				</button>
+
+				<div>
+					<Logo />
+					<h3 className="logo-text">dashboard</h3>
+				</div>
+
+				<div className="btn-container">
+					<button
+						className="btn"
+						onClick={() => console.log("show logout")}
+					>
+						<FaUserCircle />
+						john
+						<FaCaretDown />
+					</button>
+					<div className="dropdown show-dropdown">
+						<button
+							onClick={() => console.log("logout user")}
+							className="dropdown-btn"
+						>
+							logout
+						</button>
+					</div>
+				</div>
+			</div>
+		</Wrapper>
   )
 }
 
